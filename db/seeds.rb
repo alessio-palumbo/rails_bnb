414e7db1066d6dbd8b6c7b9515eee99b7e016104
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-10-11 15:22:10 +1100

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Listing.create!(title: "One bredroom", city: "Sydney", street_address: "51 Dover st", country_code: "au", bed_count: "4", bedroom_count: "3", bathroom_count: "2", description: "Quiet cozy place", night_fee_cents: 4000, cleaning_fee_cents: 6000)
+
+Listing.create!(title: "Two bredroom", city: "Brisbane", street_address: "5 Lonsdale st", country_code: "au", bed_count: "5", bedroom_count: "2", bathroom_count: "1", description: "Beautiful area", night_fee_cents: 6000, cleaning_fee_cents: 8000)
+
+Listing.create!(title: "Studio", city: "Perth", street_address: "65 Albert st", country_code: "au", bed_count: "7", bedroom_count: "3", bathroom_count: "5", description: "central", night_fee_cents: 1000, cleaning_fee_cents: 7000)
