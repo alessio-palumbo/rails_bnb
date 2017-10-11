a23f8c00f5b2d2c99d1923ab1c60a2152c8c49d4
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-10-11 12:50:00 +1100

    Add Listing, devise and and Host reference to Listing

---
+++ app/models/listing.rb
@@ -0,0 +1,10 @@
+class Listing < ApplicationRecord
+  def country
+    ISO3166::Country.new(country_code.upcase)
+  end
+
+  def full_address
+    "#{street_address}, #{city}, #{country.name}"
+  end
+  
+end
