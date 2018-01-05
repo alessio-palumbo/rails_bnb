03151d51354507a9e4fbe1e4b6ea41f5e255b75f
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2018-01-06 10:03:51 +1100

    -a

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
 
         #  , foreign_key: 'host_id', class_name: 'User'
   has_many :listings, foreign_key: 'host_id', dependent: :destroy
-  has_many :host_conversation, through: :listings, foreign_key: 'host_id', dependent: :destroy
-  has_many :guest_conversations, foreign_key: 'guest_id', dependent: :destroy
+  has_many :host_conversation, through: :listings, class_name: 'Conversation', foreign_key: 'host_id', dependent: :destroy
+  has_many :guest_conversations, class_name: 'Conversation', foreign_key: 'guest_id', dependent: :destroy
   has_many :messages, through: :host_conversations, dependent: :destroy  
   has_many :messages, through: :guest_conversations, dependent: :destroy  
 
